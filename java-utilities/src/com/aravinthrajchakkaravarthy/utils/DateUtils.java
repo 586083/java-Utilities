404ec9f37249301d66e5068fef381a276795e0d5
586083 <aravinthraj.91@gmail.com>
2020-05-29 14:25:46 +0800

    29-May:Adding DateUtils

---
+++ java-utilities/src/com/aravinthrajchakkaravarthy/utils/DateUtils.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.aravinthrajchakkaravarthy.utils;
+
+/**
+ * @author aravinthrajchakkaravarthy
+ *
+ */
+public class DateUtils {
+
+	/**
+	 * 
+	 */
+	public DateUtils() {
+		System.out.println("Inside DateUtils");
+	}
+
+}
