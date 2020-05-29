b8d6540c4210f466605db30eb3fc4fce022574c9
586083 <aravinthraj.91@gmail.com>
2020-05-29 14:47:49 +0800

    Changes

--- java-utilities/src/com/aravinthrajchakkaravarthy/utils/DateUtils.java
+++ java-utilities/src/com/aravinthrajchakkaravarthy/utils/DateUtils.java
@@ -8,12 +8,21 @@ package com.aravinthrajchakkaravarthy.utils;
 import java.util.Date;
 
 public class DateUtils {
-
+	
+	private final static String EMPTY_STRING="";
+	
 	/**
 	 * 
 	 */
 	public DateUtils() {
 		System.out.println("Inside DateUtils");
 	}
-
+	
+	public static String getDateInStringFormat(Date dateToChanged,String format) {
+		String result=EMPTY_STRING;
+		if(dateToChanged != null && format != null) {
+			
+		}
+		return result;
+	}
 }
