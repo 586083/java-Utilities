9747aa8604f7810e8fd955b5893f084e2a6ed495
586083 <aravinthraj.91@gmail.com>
2020-05-29 18:52:36 +0800

    Adding DOP calculator method

--- java-utilities/src/com/aravinthrajchakkaravarthy/utils/DateUtils.java
+++ java-utilities/src/com/aravinthrajchakkaravarthy/utils/DateUtils.java
@@ -1,5 +1,7 @@
 package com.aravinthrajchakkaravarthy.utils;
 
+import java.util.Calendar;
+
 /**
  * @author aravinthrajchakkaravarthy
  *
@@ -32,5 +34,22 @@ public class DateUtils {
 			result=df.format(dateToChanged);
 		}
 		return result;
+	}
+	
+	/**
+	 * This method convert Date to DOP
+	 * @param date
+	 * @return DOP
+	 */
+	public static int getDaysofWeek(Date date){
+		Calendar c = Calendar.getInstance();
+		c.setTime(date);
+		int day = c.get(Calendar.DAY_OF_WEEK);
+		if(day==1){
+			day = 7;
+		} else {
+			day = day-1;
+		}
+		return day;
 	}
 }
